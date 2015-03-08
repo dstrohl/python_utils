009183ff892e1b68b250900be39b32fab832c3ac
dstrohl <Kur1tacams>
2015-03-08 09:02:50 -0700

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PythonUtils',
+    version='1.0',
+    packages=['MarkupSafe', 'Pygments', 'Sphinx', 'sphinxcontrib-napoleon'],
+    url='',
+    license='',
+    author='Dan Strohl',
+    author_email='dan@wjcg.net',
+    description=''
+)
